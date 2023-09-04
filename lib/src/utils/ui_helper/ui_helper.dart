b4bd20e87ccbc145672d7dfb3b38735283bff2fd
piperfresh <sulainoabass@gmail.com>
2023-09-04 18:54:47 +0100

    Ui Helper

---
+++ lib/src/utils/ui_helper/ui_helper.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class UiHelper {
+  ///? for vertical spacing also called height
+  static const extraSmallVerticalSpacing = SizedBox(
+    height: 5,
+  );
+}
